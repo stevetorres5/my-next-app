7470b5b5ef4ec751041135f7b406c2535ef4c764
Steve Torres <torres.steve@gmail.com>
2025-08-15 13:04:23 -0700

    Enhance AWS Amplify configuration with auto branch creation and custom error handling

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,8 +20,43 @@ resource "aws_amplify_app" "app" {
   name        = "my-app"
   repository  = "https://github.com/${var.github_owner}/${var.github_repo}"
   access_token = data.aws_ssm_parameter.ssm_githubtoken.value
-  platform    = "WEB"
+  platform    = "WEB_COMPUTE"
   build_spec  = file("${path.module}/amplify.yml")
+
+  auto_branch_creation_config {
+    enable_auto_build = false
+  }
+
+  custom_rule {
+    source = "/<*>"
+    target = "/index.html"
+    status = "404"
+  }
+
+}
+
+resource "aws_amplify_branch" "main" {
+  app_id = aws_amplify_app.app.id
+  branch_name = "main"
+  enable_auto_build = true
+
+  stage = "DEVELOPMENT"
+
+  environment_variables = {
+    APP_ENV = "DEVELOPMENT"
+  }
+}
+
+resource "null_resource" "trigger_amplify_deployment" {
+  depends_on = [ aws_amplify_branch.main ]
+
+  triggers = {
+    app_id = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command = "aws amplify start-job --app-id ${aws_amplify_app.app.id} --branch-name ${aws_amplify_branch.main.branch_name} --job-type RELEASE"
+  }
 }
 
 import {
